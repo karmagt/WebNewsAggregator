b6d5710269feb479bbab334fde5ab699ad27b93b
karmagt <59287013+karmagt@users.noreply.github.com>
2021-11-21 11:06:13 +0300

    added repository implementation and context class

---
+++ WebNewsAggregator.Repositories/WebNewsAggregatorContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebNewsAggregator.Entities;
+
+namespace WebNewsAggregator.Repositories
+{
+    internal class WebNewsAggregatorContext : DbContext
+    {
+        public DbSet<WebNewsSource> WebNewsSources { get; set; }
+        public DbSet<WebNewsItem> WebNewsItems { get; set; }
+
+        public WebNewsAggregatorContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ WebNewsAggregator.Repositories/WebNewsSourceRepo.cs
@@ -0,0 +1,25 @@
+﻿using WebNewsAggregator.Entities;
+using WebNewsAggregator.Services.Interfaces.Repositories;
+
+namespace WebNewsAggregator.Repositories
+{
+    public class WebNewsSourceRepo : IWebNewsSourceRepo
+    {
+        public void Create(WebNewsSource webNewsSource)
+        {
+            using (var context = new WebNewsAggregatorContext())
+            {
+                context.WebNewsSources.Add(webNewsSource);
+                context.SaveChanges();
+            }
+        }
+
+        public WebNewsSource GetByAddress(string address)
+        {
+            using (var context = new WebNewsAggregatorContext())
+            {
+                return context.WebNewsSources.FirstOrDefault(x => x.Address == address);
+            }
+        }
+    }
+}
